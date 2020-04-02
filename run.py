3272903153692ccf998040121295ee9f745c042a
Mike <mlr314159@yahoo.co.uk>
2020-04-02 15:28:00 +0000

    tidied run.py

--- run.py
+++ run.py
@@ -9,12 +9,15 @@ def index():
     """Main page with intructions"""
     return "To send a message use /USERNAME/MESSAGE"
 
+
 @app.route('/<username>')
 def user(username):
     return "Hi " + username
 
+
 @app.route('/<username>/<message>')
 def send_message(username, message):
     return "{0}: {1}".format(username, message)
+
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
